3bccbec5f53471dab867c5ab7b3fd652ed3c02ad
rostikcheremys <rostikcheremys@gmail.com>
2023-12-03 21:34:47 +0200

    fix: errors version program

--- Program/IMyNumber.cs
+++ Program/IMyNumber.cs
@@ -2,7 +2,7 @@
 {
     public interface IMyNumber<T> where T : IMyNumber<T>
     {
-        T Add(T that);
+        T Add(T b);
         T Subtract(T b);
         T Multiply(T b);
         T Divide(T b);
